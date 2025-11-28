735a117cda51562c36cc7ddd2c9566eed0be5d7d
Usama Ahmed <umerusama871@gmail.com>
2025-11-28 15:39:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCL1937zd2Ji7EZI3z8VFqd5KEykogMZJE",
+    authDomain: "rentcars-login.firebaseapp.com",
+    projectId: "rentcars-login",
+    storageBucket: "rentcars-login.firebasestorage.app",
+    messagingSenderId: "1064235784532",
+    appId: "1:1064235784532:web:886220183ec26c66c990c1"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+
+
+  
+  
